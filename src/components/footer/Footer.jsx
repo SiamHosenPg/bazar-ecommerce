fe8a6e47dfb38873c75ef71d37b342418a8d99c8
Siam Hossen <siamhosenpg@gmail.com>
2025-02-12 22:26:34 +0600

    Footer Section Update

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -6,40 +6,40 @@ const Footer = () => {
         <div className="  bg-indigo-200 px-28 py-16 rounded-xl mb-20 mt-24">
             <div className="TopFooter flex items-start justify-between">
                 <div className="FLinks flex flex-col gap-2">
-                    <div className='mb-3'><b>Our Pages</b></div>
-                    <a href="">Company Images</a>
-                    <a href="">Blog</a>
-                    <a href="">About Us</a>
-                    <a href="">Our Socal Warker</a>
-                    <a href="">Functiions</a>
-                    <a href="">Founder</a>
+                    <div className='mb-3'><b>Products</b></div>
+                    <a href="">Featured Products</a>
+                    <a href="">Trending Now</a>
+                    <a href="">On Sale</a>
+                    <a href="">Top Rated</a>
+                    <a href="">Gift Sets</a>
+                    
                 </div>
                 <div className="FLinks flex flex-col gap-2">
-                    <div className='mb-3'><b>Our Pages</b></div>
-                    <a href="">Company Images</a>
-                    <a href="">Blog</a>
-                    <a href="">About Us</a>
-                    <a href="">Our Socal Warker</a>
-                    <a href="">Functiions</a>
-                    <a href="">Founder</a>
+                    <div className='mb-3'><b>Information</b></div>
+                    <a href="">About Our Company</a>
+                    <a href="">Privacy Policy</a>
+                    <a href="">Terms & Conditions</a>
+                    <a href="">Sitemap</a>
+                    <a href="">Security & Payment</a>
+                    
                 </div>
                 <div className="FLinks flex flex-col gap-2">
-                    <div className='mb-3'><b>Our Pages</b></div>
-                    <a href="">Company Images</a>
-                    <a href="">Blog</a>
-                    <a href="">About Us</a>
-                    <a href="">Our Socal Warker</a>
-                    <a href="">Functiions</a>
-                    <a href="">Founder</a>
+                    <div className='mb-3'><b>Support</b></div>
+                    <a href="">Customer Support</a>
+                    <a href="">Track Order</a>
+                    <a href="">Return & Exchange</a>
+                    <a href="">Warranty Info</a>
+                    <a href="">Order Help</a>
+                    
                 </div>
                 <div className="FLinks flex flex-col gap-2">
-                    <div className='mb-3'><b>Our Pages</b></div>
-                    <a href="">Company Images</a>
-                    <a href="">Blog</a>
-                    <a href="">About Us</a>
-                    <a href="">Our Socal Warker</a>
-                    <a href="">Functiions</a>
-                    <a href="">Founder</a>
+                    <div className='mb-3'><b>Stay Connected</b></div>
+                    <a href="">Newsletter Sign-Up</a>
+                    <a href="">Blog Updates</a>
+                    <a href="">Join Our Loyalty Program</a>
+                    <a href="">Exclusive Offers</a>
+                    <a href="">Upcoming Events</a>
+                    
                 </div>
             </div>
             <a className='block mt-9 text-lg font-medium' href="">View more information</a>
