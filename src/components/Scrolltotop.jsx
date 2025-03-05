fb59795d53f38b00e920ebc381527a9fc4c4a526
Shium Hossen <siamhosenpg@gmail.com>
2025-03-05 21:32:17 +0600

    Scrolling system update

--- src/components/Scrolltotop.jsx
+++ src/components/Scrolltotop.jsx
@@ -5,7 +5,19 @@ export default function ScrollToTop() {
   const { pathname } = useLocation();
 
   useEffect(() => {
-    window.scrollTo(0, 0);
+    const savedPosition = sessionStorage.getItem(`scrollPosition-${pathname}`);
+    if (savedPosition) {
+      window.scrollTo(0, parseInt(savedPosition, 10));
+    } else {
+      window.scrollTo(0, 0);
+    }
+
+    const handleScroll = () => {
+      sessionStorage.setItem(`scrollPosition-${pathname}`, window.scrollY);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
   }, [pathname]);
 
   return null;
